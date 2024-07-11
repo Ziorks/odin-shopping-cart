3a2cc916fceff15fd14d7411cb9bbf86ab17e90f
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-07-11 17:47:38 -0500

    Add content and styling to Error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,12 +1,19 @@
 import styles from "./Error.module.css";
-
+import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+import { FaRegSadCry } from "react-icons/fa";
 
 const Error = () => {
   return (
     <>
       <Navbar />
-      <div className={styles.container}>Error Page</div>;
+      <div className={styles.container}>
+        <FaRegSadCry className={styles.graphic} />
+        <p className={styles.message}>Page not found</p>
+        <Link to="/shop" className={styles.link}>
+          Continue shopping
+        </Link>
+      </div>
     </>
   );
 };
