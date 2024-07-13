042fe4dfb9415306a7d2eacc28db6d3ff4645b2c
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-07-13 00:02:03 -0500

    Removed Home page test and css files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,12 @@
 import FeaturedItems from "../../components/FeaturedItems";
 import HomeBanner from "../../components/HomeBanner";
-// import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <HomeBanner />
       <FeaturedItems />
-    </div>
+    </>
   );
 };
 
